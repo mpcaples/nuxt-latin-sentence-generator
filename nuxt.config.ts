ca77ee24b3ae0ba96aac121cf4691f20a773b2fd
madelinepc <madeline@fuegoleads.io>
2024-06-09 17:17:25 -0400

    added correct api address - please work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,9 @@ export default defineNuxtConfig({
   modules: [
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
-    'nuxt-primevue', 
-    'nuxt-icon', 
-
+    'nuxt-primevue',
+    'nuxt-icon',
+    "@nuxthub/core"
   ],
   css: ['primevue/resources/themes/lara-dark-purple/theme.css', '/assets/scss/helpme.scss']
 })
--- stores/homestore.js
+++ stores/homestore.js
@@ -9,7 +9,7 @@ export const useHomeStore = defineStore('homeStore', {
     }),
     actions: {
        async getSentence() {
-            const url = "http://127.0.0.1:5000/api/sentence"    
+            const url = "https://latin-sentence-generator.onrender.com/api/sentence"    
 
             const resp = await fetch(url)
             const data = await resp.json()
